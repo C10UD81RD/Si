4e13c5f9e98f2f6085c50bda9e67457d7bbcf2ad
C10UD81RD <100081536+C10UD81RD@users.noreply.github.com>
2023-12-23 13:37:56 +0500

    Add files via upload

---
+++ labsСИ/lab_4_10.c
@@ -0,0 +1,37 @@
+#include <math.h>
+#include <stdio.h>
+double
+f(int n)
+{
+    double intg = 0.0;
+    double h = 2.0 / n;
+    double x;
+    for(x = 1.0 / n; x <= 2.0; x += h)
+    {
+    double z;
+        if(x <= 1.0)
+            z = -cos(exp(x));
+        else
+            z = log(2 * x + sin(x * x));
+        intg += z;
+    }
+    intg *= h;
+    return intg;
+}
+int main()
+{
+    double e;
+    printf("Enter e ");
+    scanf("%lf", &e);
+    int n = 1;
+    double intg  = f(n);
+    double intg2 = f(2 * n);
+    while(fabs(intg2 - intg) / 3 >= e)
+    {
+        n = n * 2;
+        intg  = intg2;
+        intg2 = f(n);
+    }
+    printf("%lf", intg2);
+    return 0;
+}
